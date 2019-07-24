5e21f045ee217cd617c30de66796410f221c9fed
draveness <i@draveness.me>
2019-07-24 19:32:34 +0800

    test: add insert order tests for service

--- pkg/service/oceanbook/service_test.go
+++ pkg/service/oceanbook/service_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/draveness/oceanbook/api/protobuf-spec/oceanbookpb"
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
 )
 
 func TestNewOrderBook(t *testing.T) {
@@ -27,4 +28,62 @@ func TestNewOrderBook(t *testing.T) {
 	response, err = svc.NewOrderBook(context.Background(), request)
 	assert.Nil(t, err)
 	assert.Equal(t, &oceanbookpb.NewOrderBookResponse{}, response)
+}
+
+type InsertOrderServer struct {
+	grpc.ServerStream
+	trades []*oceanbookpb.Trade
+}
+
+func NewTestInsertOrderServer() *InsertOrderServer {
+	return &InsertOrderServer{
+		trades: []*oceanbookpb.Trade{},
+	}
+}
+
+func (x *InsertOrderServer) Send(t *oceanbookpb.Trade) error {
+	x.trades = append(x.trades, t)
+
+	return nil
+}
+
+func TestInsertOrder(t *testing.T) {
+	stopCh := make(chan struct{})
+	svc := NewService(stopCh)
+
+	request := &oceanbookpb.NewOrderBookRequest{
+		Pair: "BTC/CNY",
+	}
+
+	newOrderBookResponse, err := svc.NewOrderBook(context.Background(), request)
+	assert.Nil(t, err)
+	assert.Equal(t, &oceanbookpb.NewOrderBookResponse{}, newOrderBookResponse)
+
+	stream := NewTestInsertOrderServer()
+	err = svc.InsertOrder(&oceanbookpb.InsertOrderRequest{
+		Id:       1,
+		Price:    "1.0",
+		Quantity: "2.0",
+		Pair:     "BTC/CNY",
+		Side:     oceanbookpb.Order_ASK,
+	}, stream)
+	assert.Nil(t, err)
+	assert.Equal(t, []*oceanbookpb.Trade{}, stream.trades)
+
+	err = svc.InsertOrder(&oceanbookpb.InsertOrderRequest{
+		Id:       2,
+		Price:    "2.0",
+		Quantity: "1.0",
+		Pair:     "BTC/CNY",
+		Side:     oceanbookpb.Order_BID,
+	}, stream)
+	assert.Nil(t, err)
+	assert.Equal(t, []*oceanbookpb.Trade{
+		{
+			Price:    "1",
+			Quantity: "1",
+			TakerId:  2,
+			MakerId:  1,
+		},
+	}, stream.trades)
 }
